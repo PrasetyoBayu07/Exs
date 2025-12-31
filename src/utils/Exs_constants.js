0ec278a4b9c4de42cf9342c390cf379302d436eb
PrasetyoBayu07 <102822180+PrasetyoBayu07@users.noreply.github.com>
2025-12-31 16:04:23 +0700

    Create Exs_constants.js

---
+++ src/utils/Exs_constants.js
@@ -0,0 +1,17 @@
+// Exs_constants.js
+export const EXS_CONFIG = {
+  name: "EXS Digital",
+  tagline: "Innovate. Create. Elevate.",
+  description: "Solusi digital inovatif untuk bisnis masa depan",
+  contact: {
+    email: "hello@exs-digital.com",
+    phone: "+62 812-3456-7890",
+    location: "Jakarta, Indonesia"
+  },
+  social: {
+    github: "https://github.com/PrasetyoBayu07/Exs",
+    linkedin: "#",
+    twitter: "#",
+    instagram: "#"
+  }
+};
