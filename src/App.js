3d21bf37a3eb4d2595cfbef34dc0f4f9773f93f9
PrasetyoBayu07 <102822180+PrasetyoBayu07@users.noreply.github.com>
2025-12-31 17:02:26 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './styles/Exs_globals.css';  // Add this line
 import Exs_Navbar from './components/Exs_Navbar';
 import Exs_Hero from './components/Exs_Hero';
 import Exs_Services from './components/Exs_Services';
@@ -7,8 +8,11 @@ import Exs_Projects from './components/Exs_Projects';
 import Exs_About from './components/Exs_About';
 import Exs_Contact from './components/Exs_Contact';
 import Exs_Footer from './components/Exs_Footer';
+import { EXS_CONFIG } from './data/Exs_data';  // NEW: Import data
 
 function App() {
+  console.log('EXS Config:', EXS_CONFIG.site.name); // For debugging
+  
   return (
     <div className="Exs_app">
       <Exs_Navbar />
